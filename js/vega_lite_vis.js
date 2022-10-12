10cb62bb5ff3921bf90ee8e7b8f252e95ae50df5
Isaac <ilee0014@student.monash.edu>
2022-10-12 22:02:02 +0800

    feat: add all visualisation to dashboard webpage

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,4 +1,24 @@
-var spec = "js/choropleth_map.vg.json";
-vegaEmbed("#choropleth-map", spec)
+var spec1 = "js/choropleth_map.vg.json";
+vegaEmbed("#choropleth-map", spec1)
+  .then(function (result) {})
+  .catch(console.error);
+
+var spec2 = "js/happiness_life.vg.json";
+vegaEmbed("#scatterplot", spec2)
+  .then(function (result) {})
+  .catch(console.error);
+
+var spec3 = "js/regions.vg.json";
+vegaEmbed("#regions", spec3)
+  .then(function (result) {})
+  .catch(console.error);
+
+var spec4 = "js/stacked.vg.json";
+vegaEmbed("#stacked", spec4)
+  .then(function (result) {})
+  .catch(console.error);
+
+var spec5 = "js/happiness_rank.vg.json";
+vegaEmbed("#ranked", spec5)
   .then(function (result) {})
   .catch(console.error);
